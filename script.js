e36969cf9d348acc49f402104e3387a9010f1aa8
sdkamanorg-source <sdkamanorg@gmail.com>
2026-04-06 21:56:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// DEPLOY BUTTON
 function deploy(){
   window.location.href = "https://mc.auranode.qzz.io";
 }
 
-// COMING SOON
 function coming(){
   alert("Coming Soon...");
 }
 
-// TYPING
+/* SLOW TYPING */
 const text = "High performance hosting. Zero lag. Instant deploy.";
 let i = 0;
 
@@ -16,12 +14,12 @@ function typing(){
   if(i < text.length){
     document.querySelector(".typing").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing, 25);
+    setTimeout(typing, 60); // slower
   }
 }
 typing();
 
-// PARTICLES FIXED FULLSCREEN
+/* PARTICLES */
 const canvas = document.getElementById("particles");
 const ctx = canvas.getContext("2d");
 
@@ -30,17 +28,16 @@ function resize(){
   canvas.height = window.innerHeight;
 }
 resize();
-
 window.addEventListener("resize", resize);
 
 let particles = [];
 
-for(let i=0;i<80;i++){
+for(let i=0;i<100;i++){
   particles.push({
     x: Math.random()*canvas.width,
     y: Math.random()*canvas.height,
-    dx: (Math.random()-0.5)*0.5,
-    dy: (Math.random()-0.5)*0.5
+    dx:(Math.random()-0.5)*0.4,
+    dy:(Math.random()-0.5)*0.4
   });
 }
 
